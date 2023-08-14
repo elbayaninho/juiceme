f737febdda22392a4b39ff27fc3017d4d2a23861
Baimi Badjoua <baimibadjoua@yahoo.com>
2023-08-14 21:29:48 +0100

    Initial commit

---
+++ backend/backend/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path, include
+from core import views
+from rest_framework import routers
+ 
+router = routers.DefaultRouter()
+ 
+router.register(r'tasks',views.TodoView, 'task')
+ 
+urlpatterns = [
+    path('admin/', admin.site.urls),
+ 
+    path('api/', include(router.urls))
+]
