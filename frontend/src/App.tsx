eb08030f1cc066cf2bab14d5a9ab05bdd1f5ce47
Cynthia Iradukunda <c.iradukund@alustudent.com>
2023-08-23 10:37:53 +0200

    added a signin page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import {Routes, Route} from 'react-router-dom'
 import LandingPage from './pages/LandingPage'
+import SignIn from './pages/SignIn'
 
 function App() {
   return (
     <>
       <Routes>
         <Route path='/' element={<LandingPage/>}/>
+        <Route path='/signin' element={<SignIn/>}/>
       </Routes>
     </>
   )
