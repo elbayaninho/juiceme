efedae2b1c5250b4ee2ba05184d33026bc4ce7b2
Cynthia Iradukunda <c.iradukund@alustudent.com>
2023-08-23 07:58:36 +0200

    frontend setup

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,29 +1,19 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-
+import logo from "../../assets/logo.svg"
+import './index.scss'
 const NavbarParent = () => {
    return(
-      <Navbar expand="lg" className="bg-body-tertiary">
+      <Navbar expand="lg" className="bg-body-white">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Brand href="#home"><img alt='Juiceme Logo' src={logo} /></Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbar-toggle" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+          <Nav  >
+            <Nav.Link href="#home">Products</Nav.Link>
+            <Nav.Link href="#link">Contact Us</Nav.Link>
+            <Nav.Link href="signin">Sign In</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
