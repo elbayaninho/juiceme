3526cc7e4c4751c65ea178ce14ee8f23aebc3055
Baimi Badjoua <baimibadjoua@yahoo.com>
2023-08-31 23:49:42 +0100

    Update backend/auth

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -6,7 +6,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('core.urls')),
-    path('api-auth/', include('rest_framework.urls'))
 ]
 
 if settings.DEBUG:
